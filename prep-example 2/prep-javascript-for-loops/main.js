555137c3d149e2846dacf4484a31aee2f6c27ba7
Laura Craft <llcraft2@pacbell.net>
2021-02-26 20:20:32 -0800

    Completed prep-javascript-for-loops

---
+++ prep-example 2/prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+var i;
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// var i;
+for (i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+
+for (let i = 100; i >= 0; i--) {
+  console.log('Time till explosion', `${i}!`);
+
+}
